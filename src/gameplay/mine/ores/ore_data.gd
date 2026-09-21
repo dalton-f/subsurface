6383420fbee0255f4e8aa0714ec563e00c73a143
Dalton <daltonfiggdavies@gmail.com>
2026-09-21 11:22:48 +0100

    Set up main.tscn
    Create a meshlib from space_base_bits, add a light, WorldEnviroment, node hierachy organisation, add MineManager and begin to build environment

--- src/gameplay/mine/ores/ore_data.gd
+++ src/gameplay/mine/ores/ore_data.gd
@@ -5,4 +5,4 @@ extends Resource
 @export var scene: PackedScene
 
 ## The ore has a 1 in rarity chance of being selected.
-@export_range(1, 500) var rarity: int = 1
+@export_range(1, 2500) var rarity: int = 1
