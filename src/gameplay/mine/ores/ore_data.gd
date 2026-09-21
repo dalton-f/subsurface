c1e03b62170018e2c83c6846830e4dbda8a83e65
Dalton <daltonfiggdavies@gmail.com>
2026-09-21 10:21:06 +0100

    Update ore_data.gd
    Fixes rarity comment to also appear on hover in the editor

--- src/gameplay/mine/ores/ore_data.gd
+++ src/gameplay/mine/ores/ore_data.gd
@@ -4,5 +4,5 @@ extends Resource
 @export var id: StringName
 @export var scene: PackedScene
 
-# The ore has a 1 in rarity chance of being selected.
+## The ore has a 1 in rarity chance of being selected.
 @export_range(1, 500) var rarity: int = 1
