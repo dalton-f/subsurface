5cd7514d619eab8974b363ff6a2c80d60a514e80
Dalton <daltonfiggdavies@gmail.com>
2026-09-21 10:11:53 +0100

    Create layer_data and ore_data resources

---
+++ src/gameplay/mine/layers/layer_data.gd
@@ -0,0 +1,16 @@
+class_name LayerData
+extends Resource
+
+@export var layer_name: String
+@export var min_depth: int
+@export var max_depth: int
+@export var ores: Array[OreData]
+
+func get_sorted_ores() -> Array[OreData]:
+	var sorted_ores := ores.duplicate()
+	sorted_ores.sort_custom(_sort_by_rarity)
+
+	return sorted_ores
+
+func _sort_by_rarity(a: OreData, b: OreData) -> bool:
+	return a.rarity > b.rarity
---
+++ src/gameplay/mine/ores/ore_data.gd
@@ -0,0 +1,8 @@
+class_name OreData
+extends Resource
+
+@export var id: StringName
+@export var scene: PackedScene
+
+# The ore has a 1 in rarity chance of being selected.
+@export_range(1, 500) var rarity: int = 1
